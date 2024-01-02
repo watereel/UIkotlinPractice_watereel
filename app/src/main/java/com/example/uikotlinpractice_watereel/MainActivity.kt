5e0a77a9d174f92a412a15f15433e97361b67e6d
Seungbae <watereel@gmail.com>
2024-01-02 15:58:26 +0900

    Initial commit

---
+++ app/src/main/java/com/example/uikotlinpractice_watereel/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.uikotlinpractice_watereel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
